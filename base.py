668513ed8198295f831150806be9638b41b2169e
Will Roscoe <wroscoe@gmail.com>
2017-10-09 22:14:37 -0700

    update calibration and visual odometry

--- base.py
+++ base.py
@@ -15,7 +15,12 @@ import cv2
 
 get_ipython().magic('matplotlib inline')
 from matplotlib import pyplot as plt
+from matplotlib import pylab
+%pylab inline
+pylab.rcParams['figure.figsize'] = (10, 6)
+
 from PIL import Image
+
 
 
 
@@ -28,13 +33,33 @@ class default:
 
 default.camera_img_size = (640, 480)
 
-default.camera_mtx = np.array([[352.819, 0.0, 309.883], 
-                                 [0.0, 330.388, 241.680], 
-                                 [0.0, 0.0, 1.0]])
+default.camera_mtx = np.array([[ 193.06670785,    0.        ,   75.9227073 ],
+                               [   0.        ,  194.50832782,   60.13050291],
+                               [   0.        ,    0.        ,    0.25      ]])
 
 #camera distortion coefficients (k1, k2, k3, k4)
-default.camera_dist = np.array([[-0.04651275799849617], 
-                                  [-0.00205612795823227], 
-                                  [-0.017685697995019633], 
-                                  [0.012977195205127278]])
+default.camera_dist = np.array([[ -1.65260527],
+                               [  5.7104838 ],
+                               [-23.89860722],
+                               [ 55.40237623]])
 
+
+
+
+
+def show_imgs(array_plots, cmap=None):
+    """
+    Convenience function to show many images in a single plot
+    """
+    
+    plot_count = len(array_plots)
+    
+    fig, ax = plt.subplots(1, plot_count, figsize=(15, 6), squeeze=False,
+                             subplot_kw={'adjustable': 'box-forced'})
+
+    axoff = np.vectorize(lambda ax:ax.axis('off'))
+    axoff(ax)
+
+    for i, data in enumerate(array_plots):        
+        ax[0, i].imshow(data['array'], cmap=cmap)
+        ax[0, i].set_title(data.get('name', ' '))
